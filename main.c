f9da046eda2a8b2006aa7bb74a4f2857d9b8f23f
huitsept <70679674+huitsept@users.noreply.github.com>
2020-10-06 16:28:59 +0900

    td04

--- main.c
+++ main.c
@@ -4,20 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int num=0;
-	char c;
+	int i;
+	int sum;
+	int count;
+
+	printf("자연수  하나를 입력하세요: ");
+	scanf("%d",&i);
 	
-	printf("input a string: ");
-	
-	while( (c=getchar()) != '\n')
-	{
-		if ( c >= '0' && c <= '9')
-		{
-			num++;
-		}
-	}
-	
-	printf("the number of digits is %i\n", num);
-	
+	for(count=1;count<=i;count++)
+
+		sum+=count;
+	 
+	printf("1부터 입력한 정수의 합은 %d입니다.",sum);
 	return 0;
 }
